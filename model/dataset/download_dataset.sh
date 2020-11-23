59e4ea4432bbf6437d14b6372fd70d06f0bcea74
Puay Hiang <gphofficial@gmail.com>
2020-11-23 16:39:43 +0800

    streamlit

---
+++ model/dataset/download_dataset.sh
@@ -0,0 +1,7 @@
+gdown --id 1d3RkrCD8Um3jnbD6n9pk6rQ6cc3O0GIz
+unzip 'piano_only_notewise_dataset.zip'
+rm 'piano_only_notewise_dataset.zip'
+
+gdown --id 1sTYLwAaQGvV3fIvs8lOVWy2Ha8-L5m16
+unzip 'piano_violin_notewise_dataset.zip'
+rm 'piano_violin_notewise_dataset.zip'
